32687c7ab465d79f4c09fcbb1d870b5a2ce36645
starplatinum111 <5428527+starplatinum111@user.noreply.gitee.com>
2022-06-23 19:08:11 +0800

    useLibrary 'org.apache.http.legacy'

---
+++ push.cmd
@@ -0,0 +1,8 @@
+
+git init
+
+@REM git remote add origin https://gitee.com/starplatinum111/aliyunyuan-android.git
+git remote add origin https://gitee.com/starplatinum111/my-currencies2.git
+git add .
+git commit -m "useLibrary 'org.apache.http.legacy'"
+git push -u origin "master"
